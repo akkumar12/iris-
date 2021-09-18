678845f163955291b740a8df0e46c259dd62bccf
akkumar12 <90949629+akkumar12@users.noreply.github.com>
2021-09-18 12:06:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+# importing the  libraries
+import pickle 
+from flask import Flask, render_template, request
+
+# Global Variables
+loadedModel=pickle.load(open('KNN Model.pkl','rb'))
+app=Flask(__name__)
+
+
+#routers
+@app.route('/')
+def home():
+    return render_template('iris.html')
+
+@app.route('/prediction', methods=['POST'])
+def prediction():
+    SepalLengthCm = request.form['SepalLengthCm']
+    SepalWidthCm = request.form['SepalWidthCm']
+    PetalLengthCm = request.form['PetalLengthCm']
+
+
+    prediction = loadedModel.predict([[SepalLengthCm,SepalWidthCm,PetalLengthCm]])
+
+    if prediction[0] == 0:
+        prediction = "Iris-setosa"
+    elif prediction[0] == 0:
+        prediction = "Iris-versicolor"
+    else:
+        prediction = "Iris-virginica"
+
+    return render_template('iris.html', api_output=prediction)
+
+
+
+#main Function
+app.run(debug=True)
+
