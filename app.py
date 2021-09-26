20ee9a25e49597a7bba9be41605b6ec19b3f160f
akkumar12 <90949629+akkumar12@users.noreply.github.com>
2021-09-26 17:15:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,17 +14,17 @@ def home():
 
 @app.route('/prediction', methods=['POST'])
 def prediction():
-    SepalLengthCm =request.form['SepalLengthCm']
+    SepalLengthCm = request.form['SepalLengthCm']
     SepalWidthCm = request.form['SepalWidthCm']
     PetalLengthCm = request.form['PetalLengthCm']
 
 
-    prediction = loadedModel.predict([[SepalLengthcmSepalWidthCm,PetalLengthCm]])
+    prediction = loadedModel.predict([[SepalLengthCm,SepalWidthCm,PetalLengthCm]])
 
     if prediction[0] == 0:
         prediction = "Iris-setosa"
     elif prediction == 1:
-        prediction = "Iris-versicolor
+        prediction = "Iris-versicolor"
     else:
         prediction = "Iris-virginica"
 
